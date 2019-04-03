6057525d7bc9a624785142d559e05edff1b414a4
raja0612 <rajashekhar.ramayampeta@gmail.com>
2019-04-02 22:48:40 -0400

    First Commit

---
+++ InterviewPreparation/src/Arrays/ArraysSum.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArraysSum {
+
+	public static void main(String[] args) {
+
+		int a[] = {1,2,3,4,5,6,7,8,9,10};
+		System.out.println("Sum...."+Arrays.stream(a).sum());
+
+	}
+
+}
